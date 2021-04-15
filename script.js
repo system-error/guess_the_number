8ba4fc820ea74ff54f46122ac35921580697b8f6
system-error <christos_dousmanopoulos@yahoo.gr>
2021-04-15 10:31:40 +0300

    initial code

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const number = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!";
+  }
+});
+console.log(document.querySelector(".message").textContent);
