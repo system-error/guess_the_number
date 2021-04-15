fd1cca5e81669143dd7d7a611e4a4c2ccf664021
system-error <christos_dousmanopoulos@yahoo.gr>
2021-04-15 12:48:19 +0300

    finished the game

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
 "use strict";
 
-const number = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
 
-console.log();
+const displayMessage = function (theMessage) {
+  document.querySelector(".message").textContent = theMessage;
+};
+
+const displayTheNumber = function (theNumber, theNumberStyle) {
+  document.querySelector(".number").textContent = theNumber;
+  document.querySelector(".number").style.width = theNumberStyle;
+};
+
+const displayTheScore = function (theScore) {
+  document.querySelector(".score").textContent = theScore;
+};
+
+const changeBackgroundColor = function (color) {
+  document.querySelector("body").style.backgroundColor = color;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number!";
+    displayMessage("No Number!");
+  } else if (guess === secretNumber) {
+    displayMessage("Correct Number!");
+
+    displayTheNumber(secretNumber, "30rem");
+
+    changeBackgroundColor("#60b347");
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(
+        guess > secretNumber
+          ? "Grater Than Secret Number!"
+          : "Less Than Secret Number!"
+      );
+      displayTheScore(--score);
+    } else {
+      displayMessage("Lost!");
+      displayTheScore(0);
+    }
   }
 });
-console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".guess").value = "";
+  displayMessage("Start guessing...");
+
+  changeBackgroundColor("#222");
+  displayTheNumber("?", "15rem");
+
+  displayTheScore(score);
+});
